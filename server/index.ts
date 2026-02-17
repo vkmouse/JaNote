8fa9d6757807cf3bd64612600336dde73ecb1eed
vkmouse <knight31241@gmail.com>
2026-02-17 15:15:01 +0800

    feat: 測試前端

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,36 @@
-export default {
-	fetch(request) {
-		const url = new URL(request.url);
+interface Env {
+	ASSETS?: Fetcher;
+}
 
-		if (url.pathname.startsWith("/api/")) {
-			return Response.json({
-				name: "Cloudflare",
-			});
+const DEV_PROXY_ORIGIN = 'http://127.0.0.1:5173';
+
+const isHtmlRequest = (request: Request): boolean => {
+	const accept = request.headers.get('accept') ?? '';
+	return accept.includes('text/html');
+};
+
+export default {
+	async fetch(request, env) {
+		if (!env?.ASSETS) {
+			const url = new URL(request.url);
+			url.protocol = 'http:';
+			url.host = DEV_PROXY_ORIGIN.replace('http://', '');
+			return fetch(new Request(url.toString(), request));
 		}
-		return new Response(null, { status: 404 });
+
+		const response = await env.ASSETS.fetch(request);
+		if (response.status !== 404) {
+			return response;
+		}
+		if (request.method !== 'GET' && request.method !== 'HEAD') {
+			return response;
+		}
+		if (!isHtmlRequest(request)) {
+			return response;
+		}
+
+		const url = new URL(request.url);
+		url.pathname = '/index.html';
+		return env.ASSETS.fetch(new Request(url.toString(), request));
 	},
 } satisfies ExportedHandler<Env>;
