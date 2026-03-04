e4e3d82f793d82ec877d06b1fd38836f1359d419
vkmouse <knight31241@gmail.com>
2026-03-04 04:55:41 +0000

    refactor: streamline search input markup in TransactionsView

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -145,11 +145,7 @@
             <circle cx="11" cy="11" r="8" />
             <line x1="21" y1="21" x2="16.65" y2="16.65" />
           </svg>
-          <input
-            type="text"
-            class="search-input"
-            placeholder="搜尋"
-          />
+          <input type="text" class="search-input" placeholder="搜尋" />
         </div>
 
         <!-- Add Button -->
