24b17e2a62ca3f2c764741fd3ea406f354ccc6a0
vkmouse <knight31241@gmail.com>
2026-03-05 09:13:35 +0000

    feat: 更改名稱

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SyncView from '../views/SyncView.vue'
-import TransactionSummary from '../views/TransactionSummary.vue'
-import TransactionsView from '../views/TransactionsView.vue'
-import TransactionEditView from '../views/TransactionEditView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SyncView from "../views/SyncView.vue";
+import TransactionSummaryView from "../views/TransactionSummaryView.vue";
+import TransactionView from "../views/TransactionView.vue";
+import TransactionEditView from "../views/TransactionEditView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/transactions' },
-    { path: '/sync', name: 'sync', component: SyncView },
-    { path: '/transactions/summary', name: 'transaction-summary', component: TransactionSummary },
-    { path: '/transactions', name: 'transactions', component: TransactionsView },
-    { path: '/transactions/new', name: 'transaction-new', component: TransactionEditView },
-    { path: '/transaction/:id/edit', name: 'transaction-edit', component: TransactionEditView },
+    { path: "/", redirect: "/transactions" },
+    { path: "/sync", name: "sync", component: SyncView },
+    {
+      path: "/transactions/summary",
+      name: "transaction-summary",
+      component: TransactionSummaryView,
+    },
+    {
+      path: "/transactions",
+      name: "transactions",
+      component: TransactionView,
+    },
+    {
+      path: "/transactions/new",
+      name: "transaction-new",
+      component: TransactionEditView,
+    },
+    {
+      path: "/transaction/:id/edit",
+      name: "transaction-edit",
+      component: TransactionEditView,
+    },
   ],
-})
+});
 
-export default router
+export default router;
